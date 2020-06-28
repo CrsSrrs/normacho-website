196cc5b86664d2b88e1a525c31c571592ea8e691
Christoph Schroers <c.schroers@me-company.de>
2020-06-28 12:21:10 +0200

    add font, change settings, add logo

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <template>
   <div id="app" class="site" data-server-rendered="true">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="site-inner">
+      <Logo>
+        <img src="./assets/logo.jpg" alt="Normacho">
+      </Logo>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss" src="./sass/style.scss"></style>
+
+<script>
+import Logo from '@/components/atoms/Logo.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Logo,
+  },
+};
+</script>

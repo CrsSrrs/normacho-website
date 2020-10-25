66b701f21cb5913058b717262f3738051250ac12
Christoph Schroers <c.schroers@me-company.de>
2020-10-25 12:52:14 +0100

    add year to event list

--- src/components/modules/LiveList.vue
+++ src/components/modules/LiveList.vue
@@ -15,7 +15,7 @@
       <div class="_list">
         <div class="row  _item">
           <div class="col-2  col-xs-4">
-            <div class="_date">10.12.<span class="_additional">ab 19:30 Uhr</span></div>
+            <div class="_date">10.12.<span class="_year">2020</span><span class="_additional">ab 19:30 Uhr</span></div>
           </div>
           <div class="col-4  col-xs-8">
             <div class="_title">RPZ Showcase #18</div>
@@ -36,13 +36,13 @@
         </div>
         <div class="row  _item  -expired">
           <div class="col-2  col-xs-4">
-            <div class="_date">24.10.<span class="_additional">ab 21:00 Uhr</span></div>
+            <div class="_date">24.10.<span class="_year">2020</span></div>
           </div>
           <div class="col-4  col-xs-8">
             <div class="_title">Krachgarten Wesel Live Stream</div>
           </div>
           <div class="col-4  col-xs-8  col-xs-offset-4  mb-xs-xs">
-            <div class="_location">KrachgartenTV auf Twitch</div>
+            <div class="_location"><a href="https://twitch.tv/krachgartentv" target="_blank">KrachgartenTV</a> auf Twitch</div>
           </div>
           <div class="col-2  col-xs-8  col-xs-offset-4">
             <div class="_link">

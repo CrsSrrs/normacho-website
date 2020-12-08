96134d08092905e35f1f32efff7e372b4ce86ee6
Christoph Schroers <c.schroers@me-company.de>
2020-12-08 13:08:06 +0100

    update live list date

--- src/components/modules/LiveList.vue
+++ src/components/modules/LiveList.vue
@@ -15,17 +15,17 @@
       <div class="_list">
         <div class="row  _item">
           <div class="col-2  col-xs-4">
-            <div class="_date">10.12.<span class="_year">2020</span><span class="_additional">ab 19:30 Uhr</span></div>
+            <div class="_date"><s>10.12.</s><span class="_year"><s>2020</s></span><span class="_additional">Neues Datum TBA.</span></div>
           </div>
           <div class="col-4  col-xs-8">
-            <div class="_title">RPZ Showcase #18</div>
+            <div class="_title">RPZ Showcase (verschoben)</div>
           </div>
           <div class="col-4  col-xs-8  col-xs-offset-4  mb-xs-xs">
             <div class="_location">RPZ Bonn</div>
           </div>
           <div class="col-2  col-xs-8  col-xs-offset-4">
             <div class="_link">
-              <a class="link" href="https://rpz-bonn.de/events/10-12-2020-rpz-showcase-18/" target="_blank">Zum Event</a>
+              <a class="link" href="https://rpz-bonn.de/" target="_blank">Zur Location</a>
             </div>
           </div>
         </div>

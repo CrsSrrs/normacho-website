8af638dacd78003d1ce4f36439d16fe1d8590cf8
Christoph Schroers <c.schroers@me-company.de>
2024-05-25 14:35:53 +0200

    add new logo

--- src/components/modules/HeaderBar.vue
+++ src/components/modules/HeaderBar.vue
@@ -2,19 +2,12 @@
   <header class="header-bar">
     <div class="site-inner">
       <div class="row">
-        <div class="col-3  col-sm-6  col-xs-4  valign-middle">
-          <div class="row  text-center">
-            <div class="col-5  col-xs-12  valign-middle">
-              <Logo href="/" target="_self">
-                <img src="@/assets/logo_small_negative.webp" alt="Normacho">
-              </Logo>
-            </div>
-            <div class="col-7  valign-middle  hide-xs">
-              <h5 class="mb-0">Normacho</h5>
-            </div>
-          </div>
+        <div class="col-4  col-sm-6  col-xs-4  valign-middle">
+          <Logo href="/" target="_self">
+            <LogoSVG />
+          </Logo>
         </div>
-        <div class="col-9  col-sm-6  col-xs-8  valign-middle">
+        <div class="col-8  col-sm-6  col-xs-8  valign-middle">
           <nav class="main-nav  text-right">
             <router-link class="nav-link" to="/#band">Band</router-link>
             <router-link class="nav-link" to="/#music">Musik</router-link>
@@ -28,6 +21,7 @@
 
 <script setup lang="ts">
 import Logo from '@/components/atoms/Logo.vue';
+import LogoSVG from '@/assets/normacho_logo_ofont_202405.svg?component';
 </script>
 
 <style scoped lang="scss" src="@/sass/08_modules/header-bar.scss"></style>

f2f7c748424557561d4dfe5f39e79f6a3e4aa6dd
Christoph Schroers <c.schroers@me-company.de>
2024-09-16 14:10:27 +0200

    update live list

--- src/components/modules/LiveList.vue
+++ src/components/modules/LiveList.vue
@@ -20,18 +20,18 @@
         </div>
         <div class="row  _item">
           <div class="col-2  col-xs-4">
-            <div class="_date">20.07.<span class="_year">2024</span><span class="_additional">Einlass: 19:30 Uhr<br>Eintritt: 5€</span></div>
+            <div class="_date">05.10.<span class="_year">2024</span><span class="_additional">Einlass: 19 Uhr<br>Eintritt: 8€, Abendkasse only</span></div>
           </div>
           <div class="col-4  col-xs-8">
-            <div class="_title">Rhein-Rock präsentiert</div>
-            <p class="mb-0  mb-xs-xxs  mt-xxs  font-size-xs">Gemeinsam mit <a href="https://www.backseatalley.com/" target="_blank">Backseat Alley</a> und <a href="https://www.instagram.com/cleanslate.official/" target="_blank">Clean Slate</a></p>
+            <div class="_title">Cube in Concert</div>
+            <p class="mb-0  mb-xs-xxs  mt-xxs  font-size-xs">Gemeinsam mit <a href="https://www.instagram.com/dailyhavoc/" target="_blank">Daily Havoc</a></p>
           </div>
           <div class="col-4  col-xs-8  col-xs-offset-4  mb-xs-xs">
-            <div class="_location"><a href="https://maps.app.goo.gl/BEADCzi6EvyYL9nG8" target="_blank">Sojus 7</a>, Monheim</div>
+            <div class="_location"><a href="https://maps.app.goo.gl/8XqvBztuyK95Qos2A" target="_blank">Cube</a> Baumberg</div>
           </div>
           <div class="col-2  col-xs-8  col-xs-offset-4">
             <div class="_link">
-              <a class="link" href="https://rhein-rock.ticket.io/dbjjhnhd/" target="_blank">Zu den Tickets</a>
+              <a class="link" href="https://www.facebook.com/events/2244680995906414/?ref=newsfeed" target="_blank">Zum Event</a>
             </div>
           </div>
         </div>
@@ -72,6 +72,23 @@
         <div class="row">
           <div class="col-12">
             <h6 class="h6  mt-m">Vergangene Events</h6>
+          </div>
+        </div>
+        <div class="row  _item -expired">
+          <div class="col-2  col-xs-4">
+            <div class="_date">20.07.<span class="_year">2024</span><span class="_additional">Einlass: 19:30 Uhr<br>Eintritt: 5€</span></div>
+          </div>
+          <div class="col-4  col-xs-8">
+            <div class="_title">Rhein-Rock präsentiert</div>
+            <p class="mb-0  mb-xs-xxs  mt-xxs  font-size-xs">Gemeinsam mit <a href="https://www.backseatalley.com/" target="_blank">Backseat Alley</a> und <a href="https://www.instagram.com/cleanslate.official/" target="_blank">Clean Slate</a></p>
+          </div>
+          <div class="col-4  col-xs-8  col-xs-offset-4  mb-xs-xs">
+            <div class="_location"><a href="https://maps.app.goo.gl/BEADCzi6EvyYL9nG8" target="_blank">Sojus 7</a>, Monheim</div>
+          </div>
+          <div class="col-2  col-xs-8  col-xs-offset-4">
+            <div class="_link">
+              <a class="link" href="https://rhein-rock.ticket.io/dbjjhnhd/" target="_blank">Zu den Tickets</a>
+            </div>
           </div>
         </div>
         <div class="row  _item  -expired">
